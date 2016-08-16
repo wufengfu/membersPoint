afd945973ae951635a310a44c5c97efb898a717d
wufengfu <sky_fengfu@sina.com>
2016-08-16 12:28:19 +0800

    去除重要信息
    去除重要信息

--- MembersPoint/src/com/tianxiafen/cons/Constant.java
+++ MembersPoint/src/com/tianxiafen/cons/Constant.java
@@ -2,8 +2,8 @@ package com.tianxiafen.cons;
 
 public class Constant {
 
-	public final static String APPID="wxfe37491279720a57";
-	public final static String APPSECRET="dcace3fe7e0d05566827fdf8448ed908";
+	public final static String APPID="XXXXXXXXX";
+	public final static String APPSECRET="XXXXXXXXX";
 	
 	public final static String ACCOUNT="messageAccount";
 	public final static String PASSWORD="messagePassword";
